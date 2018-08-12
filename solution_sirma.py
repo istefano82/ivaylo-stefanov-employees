820146a65bfe2dde52a8ab98a1b948ad05a52f89
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-08-12 14:40:42 +0300

    Split _detect_longestteam_mates in to two functions
    One to parse file
    One to calculate pair of longest working employees

--- solution_sirma.py
+++ solution_sirma.py
@@ -1,24 +1,32 @@
 import argparse
 from collections import namedtuple
 from datetime import datetime
+import copy
 import csv
-import os
 
 
 def _convert_to_datetime_object(date_string):
-    '''Take date in string format YYYY-MM-DD and convert it to datetime object.
+    """Take date in string format YYYY-MM-DD and convert it to datetime object.
 
-    If date_string is NULL convert it to datetime object with 'today's' date.'''
+    If date_string is NULL convert it to datetime object with 'today's' date.
+    :param date_string: date in a string format YYYY-MM-DD
+    :return: datetime object
+    """
     time_format = '%Y-%m-%d'
     if date_string == 'NULL':
         return datetime.today()
     return datetime.strptime(date_string, time_format)
-    
+
+
 def _check_overlapping_times(dates_1, dates_2):
-    '''Takes two lists of tuples containing start and end dates.
+    """Takes two lists of tuples containing start and end dates.
 
     Return total overlap interval in days between any of the tuples.
-    '''
+
+    :param dates_1: List of tuples containing datetime objects
+    :param dates_2: List of tuples containing datetime objects
+    :return total_overlap:
+    """
     total_overlap = 0
     for d1_start, d1_end in dates_1:
         for d2_start, d2_end in dates_2:
@@ -30,9 +38,15 @@ def _check_overlapping_times(dates_1, dates_2):
             total_overlap += max(0, (earliest_end - latest_start).days + 1)
     return total_overlap
 
-def _detect_longestteam_mates(filepath):
+
+def _parse_file(filepath):
+    """Parses csv type file and returns parsed contents in a form suitable for further processing.
+
+    File is having the following format: EmpID, ProjectID, ,
+    :param filepath: Path to file
+    :return employee_struct: {'EmpID': {'teammates': {}, 'projects': {'10': [(datetime('DateFrom'), datetime('DateTo'))]}}}
+    """
     employee_struct = {}
-    longest_teammates = {'days': 0, 'employees': ['', '']}
     try:
         with open(filepath, 'rb') as csvfile:
             csv_reader = csv.reader(csvfile, skipinitialspace=True)
@@ -54,7 +68,18 @@ def _detect_longestteam_mates(filepath):
 
     except (TypeError, IOError):
         print "Error opening file path '{}'".format(filepath)
+    return employee_struct
 
+
+def _detect_longestteam_mates(struct):
+    """Find the pair of employees that worked together the most on the same projects.
+
+    :param struct: Dictionary returned from _parse_file function
+    :return employee_struct: Modified employee structure containing 'teammates' key for every employee
+    :return  longest_teammates: Dictionary containing the pair of employees that worked together the most and total duration
+    """
+    longest_teammates = {'days': 0, 'employees': ['', '']}
+    employee_struct = copy.deepcopy(struct)
     employee_list = employee_struct.keys()
     for emp_index, emp1 in enumerate(employee_list):
         for emp2 in employee_list[emp_index + 1:]:
@@ -71,14 +96,21 @@ def _detect_longestteam_mates(filepath):
                             if overlap > longest_teammates['days']:
                                 longest_teammates['days'] = overlap
                                 longest_teammates['employees'] = [emp1, emp2]
-    return  employee_struct, longest_teammates
+    return employee_struct, longest_teammates
 
 def main(filepath):
-    employee_struct, longest_teammates = _detect_longestteam_mates(filepath)
+    """Main function orchestrating the finding the pair of employees working together and printing it to stdout.
+
+    :param filepath: Path to input file
+    :return: 0 if execution successful
+    """
+    struct = _parse_file(filepath)
+    _, longest_teammates = _detect_longestteam_mates(struct)
     print ("The pair of employees working together the most are "
         "'{}','{}', for total of '{}' days.").format(longest_teammates['employees'][0],
                                                      longest_teammates['employees'][1],
                                                      longest_teammates['days'])
+    return 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Provide path to file')
--- solution_sirma_unittest.py
+++ solution_sirma_unittest.py
@@ -1,11 +1,11 @@
-from collections import namedtuple
 from datetime import datetime
 import solution_sirma
 import subprocess
 import unittest
-
+import os
 
 class TestSolution(unittest.TestCase):
+
 
     def test_convert_to_datetime_objects_not_null(self):
         date = '2013-11-01'
@@ -88,20 +88,35 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(expected_overlap, solution_sirma._check_overlapping_times(dates_emp1, dates_emp2))
 
     def test_detect_longest_team_mates_working_more_than_once_on_same_project(self):
+        employee_struct = {'100': {'teammates': {}, 'projects': {'10': [(datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2014, 1, 1, 0, 0), datetime(2015, 4, 27, 0, 0))]}}, '143': {'teammates': {}, 'projects': {'10': [(datetime(2011, 3, 27, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0))], '12': [(datetime(2013, 11, 1, 0, 0), datetime(2014, 1, 5, 0, 0))]}}, '218': {'teammates': {}, 'projects': {'10': [(datetime(2012, 5, 16, 0, 0), datetime(2018, 8, 12, 12, 35, 0, 313337))]}}}
         interval1_start = datetime.strptime('2009-01-01', '%Y-%m-%d')
         interval1_end = datetime.strptime('2011-04-27', '%Y-%m-%d')
         interval2_start = datetime.strptime('2014-01-01', '%Y-%m-%d')
         interval2_end = datetime.strptime('2015-04-27', '%Y-%m-%d')
-        employee_struct, longest_teammates = solution_sirma._detect_longestteam_mates('pm_table.csv')
+        employee_struct, longest_teammates = solution_sirma._detect_longestteam_mates(employee_struct)
         expected_intervals = [(interval1_start, interval1_end), (interval2_start, interval2_end)]
         expected_teammates = {'days': 879, 'employees': ['100', '143']}
         self.assertEqual(expected_intervals, employee_struct['100']['projects']['10'])
         self.assertEqual(expected_teammates, longest_teammates)
 
     def test_detect_longest_team_mates_mutiple_teammates(self):
-        employee_struct, _ = solution_sirma._detect_longestteam_mates('pm_table.csv')
+        employee_struct = {'100': {'teammates': {}, 'projects': {'10': [(datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2014, 1, 1, 0, 0), datetime(2015, 4, 27, 0, 0))]}}, '143': {'teammates': {}, 'projects': {'10': [(datetime(2011, 3, 27, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0))], '12': [(datetime(2013, 11, 1, 0, 0), datetime(2014, 1, 5, 0, 0))]}}, '218': {'teammates': {}, 'projects': {'10': [(datetime(2012, 5, 16, 0, 0), datetime(2018, 8, 12, 12, 35, 0, 313337))]}}}
+        employee_struct, _ = solution_sirma._detect_longestteam_mates(employee_struct)
         expected_teammates =  {'143': 879, '218': 482}
         self.assertEqual(expected_teammates, employee_struct['100']['teammates'])
+
+    def test_parse_file(self):
+        file_contents = '''143, 12, 2013-11-01, 2014-01-05
+                            143, 10, 2011-03-27, 2011-04-27
+                            143, 10, 2009-01-01, 2011-04-27
+                            100, 10, 2009-01-01, 2011-04-27
+                            100, 10, 2014-01-01, 2015-04-27'''
+        f_path = '/tmp/temp_table'
+        with open(f_path, 'w') as f:
+            f.write(file_contents)
+        expected_employee_struct = {'100': {'teammates': {}, 'projects': {'10': [(datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2014, 1, 1, 0, 0), datetime(2015, 4, 27, 0, 0))]}}, '143': {'teammates': {}, 'projects': {'10': [(datetime(2011, 3, 27, 0, 0), datetime(2011, 4, 27, 0, 0)), (datetime(2009, 1, 1, 0, 0), datetime(2011, 4, 27, 0, 0))], '12': [(datetime(2013, 11, 1, 0, 0), datetime(2014, 1, 5, 0, 0))]}}}
+        self.assertEqual(expected_employee_struct, solution_sirma._parse_file(f_path))
+        os.remove(f_path)
 
     def test_main_prints_longest_teammates_to_the_console(self):
         expected = "The pair of employees working together the most are '100','143', for total of '879' days.\n"
